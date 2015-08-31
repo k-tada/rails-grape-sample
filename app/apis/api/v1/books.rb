ecea0b7482edfc68d74e324ac630c9f68f37086b
k-tada <white.rv0925@gmail.com>
2015-08-31 09:39:04 +0000

    add API POST /api/v1/books

--- app/apis/api/v1/books.rb
+++ app/apis/api/v1/books.rb
@@ -8,6 +8,17 @@ module API
         get '/', jbuilder: 'api/v1/books/index' do
           @books = Book.all
         end
+
+        desc 'POST /api/v1/books'
+        params do
+          requires :title, type: String, desc: "Book title."
+          requires :author, type: String, desc: "Book author."
+        end
+        post '/' do
+          book_params = ActionController::Parameters.new(params).permit(:title, :author)
+          book = Book.new(book_params)
+          book.save
+        end
       end
     end
   end
