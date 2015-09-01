315a3a5f87b26f368534c67972af1502407f339e
k-tada <white.rv0925@gmail.com>
2015-09-01 08:32:04 +0000

    refactor API.

--- app/apis/api/v1/books.rb
+++ app/apis/api/v1/books.rb
@@ -4,6 +4,24 @@ module API
   module V1
     class Books < Grape::API
       resource :books do
+        helpers do
+
+          # Parameter check
+          # :id
+          params :id do
+            requires :id, type: Integer, desc: "Book id."
+          end
+
+          # Parameter check
+          # :attributes
+          #   - :title
+          #   - :author
+          params :attributes do
+            requires :title, type: String, desc: "Book title."
+            requires :author, type: String, desc: "Book author."
+          end
+        end
+
         desc 'GET /api/v1/books'
         get '/', jbuilder: 'api/v1/books/index' do
           @books = Book.all
@@ -11,8 +29,7 @@ module API
 
         desc 'POST /api/v1/books'
         params do
-          requires :title, type: String, desc: "Book title."
-          requires :author, type: String, desc: "Book author."
+          use :attributes
         end
         post '/' do
           book_params = ActionController::Parameters.new(params).permit(:title, :author)
@@ -22,7 +39,7 @@ module API
 
         desc 'GET /api/v1/books/:id'
         params do
-          requires :id, type: Integer, desc: "Book id."
+          use :id
         end
         get '/:id', jbuilder: 'api/v1/books/show' do
           @book = Book.find(params[:id])
@@ -30,9 +47,8 @@ module API
 
         desc 'PUT /api/v1/books/:id'
         params do
-          requires :id, type: Integer, desc: "Book id."
-          requires :title, type: String, desc: "Book title."
-          requires :author, type: String, desc: "Book author."
+          use :id
+          use :attributes
         end
         put '/:id' do
           book_params = ActionController::Parameters.new(params).permit(:title, :author)
