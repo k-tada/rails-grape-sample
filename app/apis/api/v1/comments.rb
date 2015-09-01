7cf68bfe232d029946fcb03feb4505e020828ad2
k-tada <white.rv0925@gmail.com>
2015-09-01 09:58:54 +0000

    add API POST /api/v1/books/:book_id/comments

--- app/apis/api/v1/comments.rb
+++ app/apis/api/v1/comments.rb
@@ -8,10 +8,21 @@ module API
           @book = Book.find(params[:book_id])
         end
 
+        def comment_params
+          ActionController::Parameters.new(params).permit(:body)
+        end
+
         # Parameter check
         # :book_id
         params :book_id do
           requires :book_id, type: Integer, desc: "Book id."
+        end
+
+        # Parameter check
+        # :attributes
+        #   - :body
+        params :attributes do
+          requires :body, type: String, desc: "Comment body."
         end
       end
       resource :books do
@@ -26,6 +37,15 @@ module API
             get '/', jbuilder: 'api/v1/comments/index' do
               set_book
               @comments = @book.comments
+            end
+
+            desc 'POST /api/v1/books/:book_id/comments'
+            params do
+              use :attributes
+            end
+            post '/' do
+              set_book
+              @book.comments.create(comment_params)
             end
           end
         end
